978d640a2f483c92cc131d65e88f2998d364ae5b
Akisha Abesamis <abesamisakishakhy@gmail.com>
2025-11-11 11:06:10 +0800

    COMPLETED

--- frontend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
-
-bootstrapApplication(App, {
-  providers: [provideRouter(routes), provideHttpClient()]
-});
